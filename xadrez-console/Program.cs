49261f589d396fa573f627767cf64c99ee42369e
caiokronuz <caiogabriel135@gmail.com>
2024-08-19 12:54:12 -0300

    criando a classe PosicaoXadrez para referenciar as posicoes do tabuleiro usando uma forma mais natural, por exemplo (a1, c7, b4, etc)

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -6,15 +6,9 @@ namespace xadrez_console{
     class Program{
         static void Main (string[] args){
             try{
-                Tabuleiro tab = new Tabuleiro(8,8);
-            
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0,0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1,9));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0,2));
-
-                Tela.imprimirTabuleiro(tab);
-
-                Console.ReadLine();
+                PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+                Console.WriteLine(pos);
+                Console.WriteLine(pos.ToPosicao());
             }catch(TabuleiroException e){
                 Console.WriteLine("Erro: "+ e.Message);
             }
