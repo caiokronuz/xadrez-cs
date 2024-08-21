cb2123e93b7757009a5a77c5a5099f80d0306bcd
caiokronuz <caiogabriel135@gmail.com>
2024-08-21 15:26:15 -0300

    implementando função que controla restrição de movimentos

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -10,28 +10,36 @@ namespace xadrez_console{
                 PartidaDeXadrez partida = new PartidaDeXadrez();
 
                 while(!partida.terminada){
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab);
-                    Console.WriteLine();
-                    Console.WriteLine($"Turno: {partida.turno}");
-                    Console.WriteLine($"Aguardando jogada: {partida.jogadorAtual}");
+                    try{
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab);
+                        Console.WriteLine();
+                        Console.WriteLine($"Turno: {partida.turno}");
+                        Console.WriteLine($"Aguardando jogada: {partida.jogadorAtual}");
 
-                    Console.WriteLine();
+                        Console.WriteLine();
 
-                    Console.Write("Origem: ");
-                    Posicao origem = Tela.lerPosicaoXadrez().ToPosicao();
+                        Console.Write("Origem: ");
+                        Posicao origem = Tela.lerPosicaoXadrez().ToPosicao();
 
-                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+                        partida.validarPosicaoDeOrigem(origem);
 
-                    Console.Clear();
-                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+                        bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
-                    Console.WriteLine();
+                        Console.Clear();
+                        Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
 
-                    Console.Write("Destino: ");
-                    Posicao destino = Tela.lerPosicaoXadrez().ToPosicao();
+                        Console.WriteLine();
 
-                    partida.executaMovimento(origem, destino);
+                        Console.Write("Destino: ");
+                        Posicao destino = Tela.lerPosicaoXadrez().ToPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
+
+                        partida.realizaJogada(origem, destino);
+                    }catch(TabuleiroException e){
+                        Console.WriteLine("Erro: "+ e.Message);
+                        Console.ReadLine();
+                    }
                 }
 
             }catch(TabuleiroException e){
--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -34,7 +34,7 @@ namespace xadrez{
         public void executaMovimento(Posicao origem, Posicao destino){
             Peca p = tab.retirarPeca(origem);
             p.incrementarQteMovimentos();
-            Peca pecaCaputrada = tab.retirarPeca(destino);
+            Peca pecaCapturada = tab.retirarPeca(destino);
             tab.colocarPeca(p, destino);
         }
 
@@ -49,6 +49,26 @@ namespace xadrez{
                 jogadorAtual = Cor.Preta;
             }else{
                 jogadorAtual = Cor.Branca;
+            }
+        }
+
+        public void validarPosicaoDeOrigem(Posicao pos){
+            if(tab.peca(pos) == null){
+                throw new TabuleiroException("Não existe peça na posição de origem escolhida!");
+            }
+
+            if(jogadorAtual != tab.peca(pos).cor){
+                throw new TabuleiroException("A peça de origem escolhida não é sua!");
+            }
+
+            if(!tab.peca(pos).existeMovimentosPossiveis()){
+                throw new TabuleiroException("Não há movimentos possíves para essa peça.");
+            }
+        }
+
+        public void validarPosicaoDeDestino(Posicao origem, Posicao destino){
+            if(!tab.peca(origem).podeMoverPara(destino)){
+                throw new TabuleiroException("Posição de destino inválida");
             }
         }
 
